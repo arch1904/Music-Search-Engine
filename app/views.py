f104e1aa9c21420b7588a08809f50df0602b437b
Archit Gupta <archit.941@gmail.com>
2016-10-29 00:19:28 -0500

    Fixed Hyperlink Issues

--- app/views.py
+++ app/views.py
@@ -17,9 +17,8 @@ def search():
     playlist=""
     if 'q' in request.args:
         wiki=wiki_search.search(request.args['q'])
-        playlist=spotify.get_playlist(request.args['q'])
+        playlist="<a href="+spotify.get_playlist(request.args['q'])+">here</a>"
         tweets=twitter.search(request.args['q'])
-        return "Summary:<br/>"+wiki+ "<br/><br/>Playlist at:<br/>"+playlist+ "<br/><br/>TWEETS:<br/>"+ tweets
+        return "Summary:<br/>"+wiki+ "<br/><br/>For Playlist Click "+playlist+ "<br/><br/>TWEETS:<br/>"+ tweets
     return "WRONG"
-
 
