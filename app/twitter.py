af81559873e6e3be76f325e5c814c36c4201d29b
Archit Gupta <archit.941@gmail.com>
2016-11-03 23:39:17 -0500

    Removing Keys

--- app/twitter.py
+++ app/twitter.py
@@ -3,11 +3,14 @@ import tweepy
 import sys
 reload(sys)
 sys.setdefaultencoding('utf8')
-
+f= open('Twitter_Keys.txt','r')
+key1=f.readline().strip('\n')
+key2=f.readline().strip('\n')
+key3=f.readline().strip('\n')
+key4=f.readline().strip('\n')
 def get_authenticated_API():
-    auth = tweepy.OAuthHandler('SKuaMZ6iWCkHhnQBXvDmim5WF', 'm0TkxhZ6l2UIFk3BpMh8TJgAVw45kpBxF54lnhdAfIQq9xLx7J')
-    auth.set_access_token('115824133-zV5X49gQen2nj09ci0EhhuC9HvsMsiAGl0194XUB',
-                          'j45YHjZtWPyZrETmH10QJJGfxZhdwpv2adYDJl1I3W6E2')
+    auth = tweepy.OAuthHandler(key1, key2)
+    auth.set_access_token(key3,key4)
     api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
     return api
 
@@ -18,3 +21,4 @@ def search(query):
     for tweet in tweepy.Cursor(api.search, q=query).items(20):
         l+=tweet.text+'/<br/>'
     return l
+
